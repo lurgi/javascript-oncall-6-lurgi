c3631200fb59d020173daefc1019100d8da035c5
lurgi <wahoo9040@naver.com>
2023-12-16 17:56:50 +0900

    refactor(InputView): 상수 분리

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,26 +1,28 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 const INPUT_MESSAGES = Object.freeze({
-  month: "비상 근무를 배정할 월과 시작 요일을 입력하세요> ",
-  dayWorker: "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ",
-  endWorker: "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ",
+  month: '비상 근무를 배정할 월과 시작 요일을 입력하세요> ',
+  dayWorker: '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+  endWorker: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
 });
+
+const SPERATE_STRING = ',';
 
 const InputView = Object.freeze({
   async readMonth() {
     const INPUT = await Console.readLineAsync(INPUT_MESSAGES.month);
-    const [MONTH, START_DAY] = INPUT.split(",");
+    const [MONTH, START_DAY] = INPUT.split(SPERATE_STRING);
     return [Number(MONTH), START_DAY];
   },
 
   async readDayWorker() {
     const INPUT = await Console.readLineAsync(INPUT_MESSAGES.dayWorker);
-    return INPUT.split(",");
+    return INPUT.split(SPERATE_STRING);
   },
 
   async readEndWorker() {
     const INPUT = await Console.readLineAsync(INPUT_MESSAGES.endWorker);
-    return INPUT.split(",");
+    return INPUT.split(SPERATE_STRING);
   },
 });
 
