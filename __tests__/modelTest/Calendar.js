e41c0afd814064533669fe2e2ec02731f8a05254
lurgi <wahoo9040@naver.com>
2023-12-16 17:53:44 +0900

    test(Calendar): 배열 생성 확인

--- __tests__/modelTest/Calendar.js
+++ __tests__/modelTest/Calendar.js
@@ -1,5 +1,6 @@
-import Calendar from '../../src/model/Calender';
+import Calendar from '../../src/model/Calendar';
 
+/* eslint-disable */
 describe('Calendar 테스트', () => {
   test.each([
     [13, '월'],
@@ -7,5 +8,15 @@ describe('Calendar 테스트', () => {
     [3, '월화'],
   ])('Calendar 생성 에러', (month, startDay) => {
     expect(() => new Calendar(month, startDay)).toThrow('[ERROR]');
+  });
+
+  test.each([
+    [1, '월', 31],
+    [2, '화', 28],
+    [12, '토', 31],
+  ])('getDayCalendar, getBlankCalendar', (month, startDay, expectDay) => {
+    const CALENDAR = new Calendar(month, startDay);
+    expect(CALENDAR.getDayCalendar().length).toStrictEqual(expectDay + 1);
+    expect(CALENDAR.getBlankCalendar().length).toStrictEqual(expectDay + 1);
   });
 });
