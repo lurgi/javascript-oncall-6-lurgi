ee31e4dfbc2cf9b4f2129b285d5c5300bcecfc66
lurgi <wahoo9040@naver.com>
2023-12-16 18:15:53 +0900

    feat(InputView): 입력 오류

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,23 +6,36 @@ const INPUT_MESSAGES = Object.freeze({
   endWorker: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
 });
 
+const ERROR_MESSAGE = '[ERROR] 입력하지 않으셨습니다.';
+
 const SPERATE_STRING = ',';
 
 const InputView = Object.freeze({
   async readMonth() {
     const INPUT = await Console.readLineAsync(INPUT_MESSAGES.month);
+    this.validInput(INPUT);
+
     const [MONTH, START_DAY] = INPUT.split(SPERATE_STRING);
+
     return [Number(MONTH), START_DAY];
   },
 
   async readDayWorker() {
     const INPUT = await Console.readLineAsync(INPUT_MESSAGES.dayWorker);
+    this.validInput(INPUT);
+
     return INPUT.split(SPERATE_STRING);
   },
 
   async readEndWorker() {
     const INPUT = await Console.readLineAsync(INPUT_MESSAGES.endWorker);
     return INPUT.split(SPERATE_STRING);
+  },
+
+  validInput(input) {
+    if (!input) {
+      throw new Error(ERROR_MESSAGE);
+    }
   },
 });
 
